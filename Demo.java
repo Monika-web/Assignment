252fa05a0dc595d1c7a80b0f870ccfec0eafecd4
Monika <107108026+Monika-web@users.noreply.github.com>
2023-11-21 23:14:53 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo {
+    public static void main(String args[]) {
+        int a = 3, b=40, c=30;
+        if((a>b && a>c) || (b>a && b>c))
+            System.out.println("hello");
+        else 
+            System.out.println("nothing");
+        
+        
+    }
+}

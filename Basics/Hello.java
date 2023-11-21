1a535425c92cbe702b9d72015d041087d75df926
Monika <107108026+Monika-web@users.noreply.github.com>
2023-11-21 23:18:32 +0530

    Add files via upload

---
+++ Basics/Hello.java
@@ -0,0 +1,44 @@
+    /**
+     * Hello
+     */
+    public class Hello {
+
+        public static void main(String[] args) {
+            
+            // DATA TYPES
+            // int num1=2;
+            // int num2=10;
+            // int result=num1 + num2;
+            // System.out.println(result);  
+
+            // int i=90;
+            // short sh=8;
+            // long l=11234678l;
+            // byte b=2;
+            // double d=3.86542;
+            // float ft=3.7f;
+            // char c='m';
+            // System.out.println(i);
+            // System.out.println(sh);
+            // System.out.println(l);
+            // System.out.println(b);
+            // System.out.println(d);
+            // System.out.println(ft);
+            // System.out.println(c);
+
+// TYPE CASTING:
+//             byte b1=5,b2=6;
+
+// byte b3=(byte)(b1+b2);
+
+// System.out.println(b3);
+
+
+//INCREMENT AND DECREMENT
+
+int num=3;
+int result = ++num; // first increment the value and fetch the num value output: 4
+int result = num++; // first fetch the value of num and increment output: 3
+
+        }
+    }

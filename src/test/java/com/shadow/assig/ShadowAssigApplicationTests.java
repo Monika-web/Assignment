2ce8f51fb30a245499f2db9b006c615fa28f01d0
M <monika.b.m@capgemini.com>
2023-11-22 23:11:15 +0530

    First commit

---
+++ src/test/java/com/shadow/assig/ShadowAssigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadow.assig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowAssigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
